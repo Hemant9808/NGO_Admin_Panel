c4d6a2e2b23feee7ab9c2d82338e13f5346bb0e3
Lalit-threely <147034093+Lalit-threely@users.noreply.github.com>
2024-09-02 22:13:08 +0530

    fix

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import logo from '../assets/adiray_logo.png';
+
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
